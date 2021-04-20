ac99cda1affdde374558bc42fd6f582a15e6bcac
Cyberknecht <cyberknecht@chaos-hip.de>
2021-04-20 21:29:59 +0200

    Add first Part model fields

--- models/part.go
+++ models/part.go
@@ -1,4 +1,15 @@
 package models
 
 type Part struct {
+	Name        string   `json:"name" db:"name"`
+	Description string   `json:"description" db:"description"`
+	Comment     string   `json:"comment" db:"comment"`
+	Category    Category `json:"category"`
+}
+
+type Category struct {
+	InternalID       int    `db:"id" json:"-"`
+	InternalParentID int    `db:"parent_id" json:"-"`
+	Name             string `json:"name" db:"name"`
+	Path             string `json:"path" db:"path"`
 }
