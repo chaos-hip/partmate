a6f201af0ee70463e94d60c862567bc3c0b136ab
etwas42 <git@etwas42.de>
2021-04-23 18:38:06 +0200

    Added Attchment Parameters

--- models/attachment.go
+++ models/attachment.go
@@ -1,4 +1,10 @@
 package models
 
 type Attachment struct {
+	InternalID int    `json:"-" db:"id"`
+	PartId     int    `json:"partid" db:"part_id"`
+	FileName   string `json:"filename" db:"filename"`
+	MimeType   string `json:"mimetype" db:"mimetype"`
+	Size       int    `json:"size" db:"size"`
+	IsImage    *bool  `json:"isimage" db:"isimage"`
 }
