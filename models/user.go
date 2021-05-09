396d591fada37ef39a53f1ab95569f1338752b15
etwas42 <git@etwas42.de>
2021-05-09 15:33:12 +0200

    Added Struct-Var-Data and Comments

--- models/user.go
+++ models/user.go
@@ -1,4 +1,11 @@
 package models
 
+// User is the Struct to identify the User (Requests and Cretentials)
 type User struct {
+	UserID   string `json:"-" db:"id"`
+	Username string `json:"username" db:"username"`
+	Enabled  *bool  `json:"enabled" db:"enabled"`
+	Password string `json:"-" db:"password"` // Swordfish
+	Salt     string `json:"-" db:"salt"`
+	Locked   *bool  `json:"locked" db:"locked"`
 }
