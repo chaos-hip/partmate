0941697c2981bfba04a17627aee6e61087720aca
etwas42 <mygit@etwas42.de>
2021-05-09 15:15:20 +0200

    Added Comment and renamed PartID to satisfy the Linter

--- models/attachment.go
+++ models/attachment.go
@@ -1,8 +1,9 @@
 package models
 
+// Attachment is the Struct to identify and modify Attachment-Data
 type Attachment struct {
 	InternalID int    `json:"-" db:"id"`
-	PartId     int    `json:"partid" db:"part_id"`
+	PartID     int    `json:"partid" db:"part_id"`
 	FileName   string `json:"filename" db:"filename"`
 	MimeType   string `json:"mimetype" db:"mimetype"`
 	Size       int    `json:"size" db:"size"`
