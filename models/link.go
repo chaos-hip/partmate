02e000a65f8f1523b3aa7504fd751821ccc94a7b
etwas42 <git@etwas42.de>
2021-05-09 15:52:23 +0200

    Added Struct Vars

--- models/link.go
+++ models/link.go
@@ -1,4 +1,7 @@
 package models
 
+//Link contains the URL for part-lookup (Table: mate_links)
 type Link struct {
+	PartID  string `json:"partID" db:"partID"`
+	PartURL string `json:"link" db:"link"`
 }
