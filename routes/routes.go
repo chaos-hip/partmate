a860692dc5d69eb7ec9cb7f5914b04fd3856d1ca
Cyberknecht <cyberknecht@chaos-hip.de>
2022-01-06 02:05:29 +0100

    Add default file handler for unknown paths
    This one should always return the index.html file

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"net/http"
+	"path/filepath"
+	"strings"
 
 	"git.chaos-hip.de/RepairCafe/PartMATE/errors"
 	"github.com/gin-gonic/gin"
@@ -9,6 +11,11 @@ import (
 
 // Default404Handler is a gin handler function that returns a JSON error for 404 pages
 func Default404Handler(c *gin.Context) {
+	if strings.HasPrefix(c.FullPath(), "/ui/") {
+		// Just deliver the index.html page
+		c.File(filepath.Join(".", "public", "index.html"))
+		return
+	}
 	c.AbortWithStatusJSON(http.StatusNotFound, errors.NewResponse(
 		"ENDPOINT_NOT_FOUND",
 		"This is not the endpoint you are searching for",
